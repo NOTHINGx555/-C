7961f6d8aa255ad34589d274bb9acb2a7f3c4c86
NOTHINGx555 <170884425+NOTHINGx555@users.noreply.github.com>
2024-10-18 23:49:51 +0200

    Update .lua

--- .lua
+++ .lua
@@ -1,2 +1,50 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/NOTHINGx555/4ff86776d3c489d96be536479bf28ed7/raw/9c0f504949bf7ae16b2ba2f1106841667ba47c2d/.lua"))()
-warn "SAD"
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local function onChatted(message)
+    -- Sprawdzenie wiadomości czatu (nieważne, czy wielkie litery)
+    if message:lower() == "w nothing" then
+        print("loding... script")
+        -- Pobieranie zewnętrznego skryptu i jego wykonanie
+        local success, err = pcall(function()
+            loadstring(game:HttpGet("https://gist.githubusercontent.com/NOTHINGx555/4ff86776d3c489d96be536479bf28ed7/raw/9c0f504949bf7ae16b2ba2f1106841667ba47c2d/.lua"))()
+        end)
+
+        if not success then
+            warn("Błąd podczas wykonywania skryptu: " .. err)
+        end
+    end
+end
+
+-- Nasłuchiwanie wiadomości czatu od lokalnego gracza
+LocalPlayer.Chatted:Connect(onChatted)
+warn ("type w nothing in chat")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
+warn ("w nothing")
