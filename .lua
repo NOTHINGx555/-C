cbc67b2aec8a47394f3ffdbde824818cb5b470e0
NOTHINGx555 <170884425+NOTHINGx555@users.noreply.github.com>
2024-10-18 23:17:48 +0200

    Update .lua

--- .lua
+++ .lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/NOTHINGx555/4ff86776d3c489d96be536479bf28ed7/raw/891b6002e5b36b803763c5b4946f0ea034392560/.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/NOTHINGx555/4ff86776d3c489d96be536479bf28ed7/raw/9c0f504949bf7ae16b2ba2f1106841667ba47c2d/.lua"))()
+warn "SAD"
